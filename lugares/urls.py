01ffd40316cd5bc593977e5db54815eceeeba4e4
Andre Kesselring <andre.kesselring@usp.br>
2023-11-21 03:48:21 -0300

    Classes genéricas aplicadas, finalizando a terceira parte com o CRUD

--- lugares/urls.py
+++ lugares/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from . import views
 
+from .views import PostListView, PostSearchView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
 app_name = 'lugares'
 urlpatterns = [
-    path('', views.list_lugares, name='index'),
-    path('search/', views.search_lugares, name='search'), 
-    path('<int:lugar_id>/', views.detail_lugar, name='detail'),
-    path('create/', views.create_lugar, name='create'), 
-    path('update/<int:lugar_id>/', views.update_lugar, name='update'),
-    path('delete/<int:lugar_id>/', views.delete_lugar, name='delete'),
+    path('', views.PostListView.as_view(), name='index'), 
+    path('search/', views.PostSearchView.as_view(), name='search'),  
+    path('<int:lugar_id>/', views.PostDetailView.as_view(), name='detail'), 
+    path('create/', views.PostCreateView.as_view(), name='create'), 
+    path('<int:pk>/update/', views.PostUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete'),
 ]
--- lugares/views.py
+++ lugares/views.py
@@ -6,81 +6,58 @@ from django.http import HttpResponseRedirect
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.views import generic
+from django.views.generic import DetailView, ListView, CreateView , UpdateView , DeleteView
+from django.urls import reverse_lazy
+
+
 
 # Create your views here.
 
-def detail_lugar(request, lugar_id):
-    lugar = lugar_data[lugar_id - 1]
-    return HttpResponse(
-        f'Detalhes do lugar {lugar["name"]} ({lugar["description"]})')
+class PostListView(ListView):
+    model = Post
+    template_name = 'lugares/index.html'
+    context_object_name = 'lugar_list'
 
-def list_lugares(request):
-    context = {"lugar_list": lugar_data}
-    return render(request, 'lugares/index.html', context)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'lugares/detail.html'
+    context_object_name = 'lugar'
+    pk_url_kwarg = 'lugar_id'
 
-def detail_lugar(request, lugar_id):
-    lugar = get_object_or_404( Post , pk=lugar_id)
-    context = {'lugar': lugar}
-    return render(request, 'lugares/detail.html', context)
+class PostSearchView(ListView):
+    model = Post
+    template_name = 'lugares/search.html'
+    context_object_name = 'lugar_list'
 
+    def get_queryset(self):
+        query = self.request.GET.get('query', '')
+        if query:
+            return Post.objects.filter(name__icontains=query.lower())
+        else:
+            return Post.objects.none()
 
-def search_lugares(request):
-    context = {}
-    if request.GET.get('query', False):
-        search_term = request.GET['query'].lower()
-        lugar_list = Post.objects.filter(name__icontains=search_term)
-        context = {"lugar_list": lugar_list}
-    return render(request, 'lugares/search.html', context)
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'lugares/create.html'
+    success_url = reverse_lazy('lugares:create')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return HttpResponseRedirect(reverse('lugares:detail', args=(self.object.id,)))
 
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'lugares/update.html'
+    context_object_name = 'lugar'
 
-def create_lugar(request):
-    if request.method == 'POST':
-        lugar_name = request.POST['name']
-        lugar_description = request.POST['description']
-        lugar_poster_url = request.POST['poster_url']
-        lugar_content = request.POST['content']
-        lugar = Post(name=lugar_name,
-                      description = lugar_description,
-                      poster_url = lugar_poster_url,
-                      content = lugar_content
-                      )
-        lugar.save()
-        return HttpResponseRedirect(
-            reverse('lugares:detail', args=(lugar.id, )))
-    else:
-        form = PostForm()
-        context = {'form': form}
-        return render(request, 'lugares/create.html', context)
+    def get_success_url(self):
+        return reverse_lazy('lugares:detail', args=(self.object.id,))
 
-    
-def list_lugares(request):
-    lugar_list = Post.objects.all()
-    context = {'lugar_list': lugar_list}
-    return render(request, 'lugares/index.html', context)
-
-def update_lugar(request, lugar_id):
-    lugar = get_object_or_404(Post, pk=lugar_id)
-
-    if request.method == "POST":
-        lugar.name = request.POST['name']
-        lugar.description = request.POST['description']
-        lugar.content = request.POST['content']
-        lugar.poster_url = request.POST['poster_url']
-        lugar.save()
-        return HttpResponseRedirect(
-            reverse('lugares:detail', args=(lugar.id, )))
-
-    context = {'lugar': lugar}
-    return render(request, 'lugares/update.html', context)
-
-
-def delete_lugar(request, lugar_id):
-    lugar = get_object_or_404(Post, pk=lugar_id)
-
-    if request.method == "POST":
-        lugar.delete()
-        return HttpResponseRedirect(reverse('lugares:index'))
-
-    context = {'lugar': lugar}
-    return render(request, 'lugares/delete.html', context)
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'lugares/delete.html'
+    context_object_name = 'lugar'
+    success_url = reverse_lazy('lugares:index')
