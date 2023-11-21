4f3dd8fb6b057cc22720d3e9f26cf2e706780cea
Andre Kesselring <andre.kesselring@usp.br>
2023-11-21 00:50:15 -0300

    Create básico criado

--- lugares/models.py
+++ lugares/models.py
@@ -1,23 +1,16 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
 
-class Lugar(models.Model):
+class Post(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=500)
     poster_url = models.URLField(max_length=200, null=True)
+    content = models.TextField(default="")
+    creation_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f'{self.name} ({self.description})'
+        return f'"{self.name}" - {self.description}'
 
-
-class Review(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,
-                               on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
-    likes = models.IntegerField(default=0)
-    lugar = models.ForeignKey(Lugar, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'"{self.text}" - {self.author.username}'
--- lugares/views.py
+++ lugares/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .temp_data import lugar_data
 from django.urls import reverse
 from django.http import HttpResponseRedirect
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -16,30 +18,39 @@ def list_lugares(request):
     return render(request, 'lugares/index.html', context)
 
 def detail_lugar(request, lugar_id):
-    context = {'lugar': lugar_data[lugar_id - 1]}
+    lugar = get_object_or_404( Post , pk=lugar_id)
+    context = {'lugar': lugar}
     return render(request, 'lugares/detail.html', context)
+
 
 def search_lugares(request):
     context = {}
     if request.GET.get('query', False):
-        context = {
-            "lugar_list": [
-                m for m in lugar_data
-                if request.GET['query'].lower() in m['name'].lower()
-            ]
-        }
-    return render(request, 'lugares/search.html', context) # modifique esta linha
+        search_term = request.GET['query'].lower()
+        lugar_list = Post.objects.filter(name__icontains=search_term)
+        context = {"lugar_list": lugar_list}
+    return render(request, 'lugares/search.html', context)
 
 
 
 def create_lugar(request):
     if request.method == 'POST':
-        lugar_data.append({
-            'name': request.POST['name'],
-            'description': request.POST['description'],
-            'image_url': request.POST['image_url']
-        })
+        lugar_name = request.POST['name']
+        lugar_description = request.POST['description']
+        lugar_poster_url = request.POST['poster_url']
+        lugar_content = request.POST['content']
+        lugar = Post(name=lugar_name,
+                      description = lugar_description,
+                      poster_url = lugar_poster_url,
+                      content = lugar_content
+                      )
+        lugar.save()
         return HttpResponseRedirect(
-            reverse('lugares:detail', args=(len(lugar_data), )))
+            reverse('lugares:detail', args=(lugar.id, )))
     else:
         return render(request, 'lugares/create.html', {})
+    
+def list_lugares(request):
+    lugar_list = Post.objects.all()
+    context = {'lugar_list': lugar_list}
+    return render(request, 'lugares/index.html', context)
